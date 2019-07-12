ee88a767ea0034a4d2e2afffaa450996e5673dba
seda <seda.t@rashensoft.com>
2019-07-12 13:57:03 +0300

    adding changes

--- main.c
+++ main.c
@@ -11,15 +11,16 @@
 
 //Task1
 
-int decToBinfunc(int a){
+void decToBinfunc(int a){
    
   int b = a%2;
-    
+  char p[100];
+  sprintf(p, "%d", b);
     if (a>0){
         
     decToBinfunc(a/2);
      
-   printf("%d",b);
+   printf("%d",*p);
         
     }
    
@@ -35,9 +36,7 @@ int oddEvenPowerRec(int a,int b){
     if (b==0){
         return 1;
     }
-    if (b==1){
-        return a;
-    }
+   
     if (b % 2 == 0) {
         k  = oddEvenPowerRec(a, b / 2);
         return k * k;
